7456cb06ed8a2f15d61086e7e6bc1cfc4f1d48a4
christoseteoglou <eatoldglue@DESKTOP-ABR6L0P.localdomain>
2021-05-18 18:14:27 +0200

    https errors

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express()
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 // this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-	res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
+// app.get(/.*/, function (req, res) {
+// 	res.sendFile(path.join(__dirname, '/dist/index.html'))
+// })
 
 app.get('/', function(req, res, next) {
     if (req.protocol == 'http') {
